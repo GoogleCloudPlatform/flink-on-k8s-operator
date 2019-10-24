02104206a6f3f375422e59feca1d341eb55eb014
Dagang Wei <functicons@gmail.com>
2019-10-24 11:38:03 -0700

    Add validation for create FlinkCluster requests

--- api/v1alpha1/flinkcluster_validate.go
+++ api/v1alpha1/flinkcluster_validate.go
@@ -17,16 +17,170 @@ limitations under the License.
 package v1alpha1
 
 import (
-	"errors"
+	"fmt"
 	"reflect"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// Validates update request.
-func validateUpdate(old *FlinkCluster, new *FlinkCluster) error {
+// Validator validates CUD requests for the CR.
+type Validator struct{}
+
+// ValidateCreate validates create request.
+func (v *Validator) ValidateCreate(cluster *FlinkCluster) error {
+	var err error
+	err = v.validateMeta(&cluster.ObjectMeta)
+	if err != nil {
+		return err
+	}
+	err = v.validateImage(&cluster.Spec.Image)
+	if err != nil {
+		return err
+	}
+	err = v.validateJobManager(&cluster.Spec.JobManager)
+	if err != nil {
+		return err
+	}
+	err = v.validateTaskManager(&cluster.Spec.TaskManager)
+	if err != nil {
+		return err
+	}
+	err = v.validateJob(cluster.Spec.Job)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// ValidateUpdate validates update request.
+func (v *Validator) ValidateUpdate(old *FlinkCluster, new *FlinkCluster) error {
 	if !reflect.DeepEqual(new.Spec, old.Spec) {
-		return errors.New(
+		return fmt.Errorf(
 			"updating FlinkCluster spec is not allowed," +
 				" please delete the resouce and recreate")
+	}
+	return nil
+}
+
+func (v *Validator) validateMeta(meta *metav1.ObjectMeta) error {
+	if len(meta.Name) == 0 {
+		return fmt.Errorf("cluster name is unspecified")
+	}
+	if len(meta.Namespace) == 0 {
+		return fmt.Errorf("cluster namesapce is unspecified")
+	}
+	return nil
+}
+
+func (v *Validator) validateImage(imageSpec *ImageSpec) error {
+	if len(imageSpec.Name) == 0 {
+		return fmt.Errorf("image name is unspecified")
+	}
+	switch imageSpec.PullPolicy {
+	case corev1.PullAlways:
+	case corev1.PullIfNotPresent:
+	case corev1.PullNever:
+	default:
+		return fmt.Errorf("invalid image pullPolicy: %v", imageSpec.PullPolicy)
+	}
+	return nil
+}
+
+func (v *Validator) validateJobManager(jmSpec *JobManagerSpec) error {
+	var err error
+
+	// Replicas.
+	if jmSpec.Replicas == nil || *jmSpec.Replicas != 1 {
+		return fmt.Errorf("invalid JobManager replicas, it must be 1")
+	}
+
+	// AccessScope.
+	switch jmSpec.AccessScope {
+	case AccessScope.Cluster:
+	case AccessScope.VPC:
+	case AccessScope.External:
+	default:
+		return fmt.Errorf("invalid JobManager access scope: %v", jmSpec.AccessScope)
+	}
+
+	// Ports.
+	err = v.validatePort(jmSpec.Ports.RPC, "rpc", "jobmanager")
+	if err != nil {
+		return err
+	}
+	err = v.validatePort(jmSpec.Ports.Blob, "blob", "jobmanager")
+	if err != nil {
+		return err
+	}
+	err = v.validatePort(jmSpec.Ports.Query, "query", "jobmanager")
+	if err != nil {
+		return err
+	}
+	err = v.validatePort(jmSpec.Ports.UI, "ui", "jobmanager")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (v *Validator) validateTaskManager(tmSpec *TaskManagerSpec) error {
+	// Replicas.
+	if tmSpec.Replicas < 1 {
+		return fmt.Errorf("invalid TaskManager replicas, it must >= 1")
+	}
+
+	// Ports.
+	var err error
+	err = v.validatePort(tmSpec.Ports.RPC, "rpc", "taskmanager")
+	if err != nil {
+		return err
+	}
+	err = v.validatePort(tmSpec.Ports.Data, "data", "taskmanager")
+	if err != nil {
+		return err
+	}
+	err = v.validatePort(tmSpec.Ports.Query, "query", "taskmanager")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (v *Validator) validateJob(jobSpec *JobSpec) error {
+	if jobSpec == nil {
+		return nil
+	}
+	if len(jobSpec.JarFile) == 0 {
+		return fmt.Errorf("job jarFile is unspecified")
+	}
+	if jobSpec.Parallelism == nil {
+		return fmt.Errorf("job parallelism is unspecified")
+	}
+	if *jobSpec.Parallelism < 1 {
+		return fmt.Errorf("job parallelism must be >= 1")
+	}
+	if jobSpec.RestartPolicy != nil {
+		switch *jobSpec.RestartPolicy {
+		case corev1.RestartPolicyNever:
+		case corev1.RestartPolicyOnFailure:
+		default:
+			return fmt.Errorf("invalid job restartPolicy: %v", *jobSpec.RestartPolicy)
+		}
+	}
+	return nil
+}
+
+func (v *Validator) validatePort(
+	port *int32, name string, component string) error {
+	if port == nil {
+		return fmt.Errorf("%v %v port is unspecified", component, name)
+	}
+	if *port <= 1024 {
+		return fmt.Errorf(
+			"invalid %v %v port: %v, must be > 1024", component, name, *port)
 	}
 	return nil
 }
--- api/v1alpha1/flinkcluster_validate_test.go
+++ api/v1alpha1/flinkcluster_validate_test.go
@@ -20,21 +20,395 @@ import (
 	"testing"
 
 	"gotest.tools/assert"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// Tests updating status is allowed.
+func TestValidateCreate(t *testing.T) {
+	var jmReplicas int32 = 1
+	var rpcPort int32 = 8001
+	var blobPort int32 = 8002
+	var queryPort int32 = 8003
+	var uiPort int32 = 8004
+	var dataPort int32 = 8005
+	var parallelism int32 = 2
+	var restartPolicy = corev1.RestartPolicyOnFailure
+	var cluster = FlinkCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "mycluster",
+			Namespace: "default",
+		},
+		Spec: FlinkClusterSpec{
+			Image: ImageSpec{
+				Name:       "flink:1.8.1",
+				PullPolicy: corev1.PullPolicy("Always"),
+			},
+			JobManager: JobManagerSpec{
+				Replicas:    &jmReplicas,
+				AccessScope: AccessScope.VPC,
+				Ports: JobManagerPorts{
+					RPC:   &rpcPort,
+					Blob:  &blobPort,
+					Query: &queryPort,
+					UI:    &uiPort,
+				},
+			},
+			TaskManager: TaskManagerSpec{
+				Replicas: 3,
+				Ports: TaskManagerPorts{
+					RPC:   &rpcPort,
+					Data:  &dataPort,
+					Query: &queryPort,
+				},
+			},
+			Job: &JobSpec{
+				JarFile:       "gs://my-bucket/myjob.jar",
+				Parallelism:   &parallelism,
+				RestartPolicy: &restartPolicy,
+			},
+		},
+	}
+	var validator = &Validator{}
+	var err = validator.ValidateCreate(&cluster)
+	assert.NilError(t, err, "create validation failed unexpectedly")
+}
+
+func TestInvalidImageSpec(t *testing.T) {
+	var validator = &Validator{}
+
+	var cluster = FlinkCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "mycluster",
+			Namespace: "default",
+		},
+		Spec: FlinkClusterSpec{},
+	}
+	var err = validator.ValidateCreate(&cluster)
+	var expectedErr = "image name is unspecified"
+	assert.Equal(t, err.Error(), expectedErr)
+
+	cluster = FlinkCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "mycluster",
+			Namespace: "default",
+		},
+		Spec: FlinkClusterSpec{
+			Image: ImageSpec{
+				Name:       "flink:1.8.1",
+				PullPolicy: corev1.PullPolicy("XXX"),
+			},
+		},
+	}
+	err = validator.ValidateCreate(&cluster)
+	expectedErr = "invalid image pullPolicy: XXX"
+	assert.Equal(t, err.Error(), expectedErr)
+}
+
+func TestInvalidJobManagerSpec(t *testing.T) {
+	var jmReplicas1 int32 = 1
+	var jmReplicas2 int32 = 2
+	var rpcPort int32 = 8001
+	var blobPort int32 = 8002
+	var queryPort int32 = 8003
+	var uiPort int32 = 8004
+
+	var cluster = FlinkCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "mycluster",
+			Namespace: "default",
+		},
+		Spec: FlinkClusterSpec{
+			Image: ImageSpec{
+				Name:       "flink:1.8.1",
+				PullPolicy: corev1.PullPolicy("Always"),
+			},
+			JobManager: JobManagerSpec{
+				Replicas:    &jmReplicas2,
+				AccessScope: AccessScope.VPC,
+				Ports: JobManagerPorts{
+					RPC:   &rpcPort,
+					Blob:  &blobPort,
+					Query: &queryPort,
+					UI:    &uiPort,
+				},
+			},
+		},
+	}
+	var err = validator.ValidateCreate(&cluster)
+	var expectedErr = "invalid JobManager replicas, it must be 1"
+	assert.Equal(t, err.Error(), expectedErr)
+
+	cluster = FlinkCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "mycluster",
+			Namespace: "default",
+		},
+		Spec: FlinkClusterSpec{
+			Image: ImageSpec{
+				Name:       "flink:1.8.1",
+				PullPolicy: corev1.PullPolicy("Always"),
+			},
+			JobManager: JobManagerSpec{
+				Replicas:    &jmReplicas1,
+				AccessScope: "XXX",
+				Ports: JobManagerPorts{
+					RPC:   &rpcPort,
+					Blob:  &blobPort,
+					Query: &queryPort,
+					UI:    &uiPort,
+				},
+			},
+		},
+	}
+	err = validator.ValidateCreate(&cluster)
+	expectedErr = "invalid JobManager access scope: XXX"
+	assert.Equal(t, err.Error(), expectedErr)
+
+	cluster = FlinkCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "mycluster",
+			Namespace: "default",
+		},
+		Spec: FlinkClusterSpec{
+			Image: ImageSpec{
+				Name:       "flink:1.8.1",
+				PullPolicy: corev1.PullPolicy("Always"),
+			},
+			JobManager: JobManagerSpec{
+				Replicas:    &jmReplicas1,
+				AccessScope: AccessScope.VPC,
+				Ports: JobManagerPorts{
+					RPC:   nil,
+					Blob:  &blobPort,
+					Query: &queryPort,
+					UI:    &uiPort,
+				},
+			},
+		},
+	}
+	err = validator.ValidateCreate(&cluster)
+	expectedErr = "jobmanager rpc port is unspecified"
+	assert.Equal(t, err.Error(), expectedErr)
+}
+
+func TestInvalidTaskManagerSpec(t *testing.T) {
+	var jmReplicas int32 = 1
+	var rpcPort int32 = 8001
+	var blobPort int32 = 8002
+	var queryPort int32 = 8003
+	var uiPort int32 = 8004
+	var dataPort int32 = 8005
+
+	var cluster = FlinkCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "mycluster",
+			Namespace: "default",
+		},
+		Spec: FlinkClusterSpec{
+			Image: ImageSpec{
+				Name:       "flink:1.8.1",
+				PullPolicy: corev1.PullPolicy("Always"),
+			},
+			JobManager: JobManagerSpec{
+				Replicas:    &jmReplicas,
+				AccessScope: AccessScope.VPC,
+				Ports: JobManagerPorts{
+					RPC:   &rpcPort,
+					Blob:  &blobPort,
+					Query: &queryPort,
+					UI:    &uiPort,
+				},
+			},
+			TaskManager: TaskManagerSpec{
+				Replicas: 0,
+				Ports: TaskManagerPorts{
+					RPC:   &rpcPort,
+					Data:  &dataPort,
+					Query: &queryPort,
+				},
+			},
+		},
+	}
+	var err = validator.ValidateCreate(&cluster)
+	var expectedErr = "invalid TaskManager replicas, it must >= 1"
+	assert.Equal(t, err.Error(), expectedErr)
+
+	cluster = FlinkCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "mycluster",
+			Namespace: "default",
+		},
+		Spec: FlinkClusterSpec{
+			Image: ImageSpec{
+				Name:       "flink:1.8.1",
+				PullPolicy: corev1.PullPolicy("Always"),
+			},
+			JobManager: JobManagerSpec{
+				Replicas:    &jmReplicas,
+				AccessScope: AccessScope.VPC,
+				Ports: JobManagerPorts{
+					RPC:   &rpcPort,
+					Blob:  &blobPort,
+					Query: &queryPort,
+					UI:    &uiPort,
+				},
+			},
+			TaskManager: TaskManagerSpec{
+				Replicas: 1,
+				Ports: TaskManagerPorts{
+					RPC:   &rpcPort,
+					Data:  &dataPort,
+					Query: nil,
+				},
+			},
+		},
+	}
+	err = validator.ValidateCreate(&cluster)
+	expectedErr = "taskmanager query port is unspecified"
+	assert.Equal(t, err.Error(), expectedErr)
+}
+
+func TestInvalidJobSpec(t *testing.T) {
+	var jmReplicas int32 = 1
+	var rpcPort int32 = 8001
+	var blobPort int32 = 8002
+	var queryPort int32 = 8003
+	var uiPort int32 = 8004
+	var dataPort int32 = 8005
+	var restartPolicy = corev1.RestartPolicyOnFailure
+	var invalidRestartPolicy = corev1.RestartPolicy("XXX")
+	var validator = &Validator{}
+	var parallelism int32 = 2
+
+	var cluster = FlinkCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "mycluster",
+			Namespace: "default",
+		},
+		Spec: FlinkClusterSpec{
+			Image: ImageSpec{
+				Name:       "flink:1.8.1",
+				PullPolicy: corev1.PullPolicy("Always"),
+			},
+			JobManager: JobManagerSpec{
+				Replicas:    &jmReplicas,
+				AccessScope: AccessScope.VPC,
+				Ports: JobManagerPorts{
+					RPC:   &rpcPort,
+					Blob:  &blobPort,
+					Query: &queryPort,
+					UI:    &uiPort,
+				},
+			},
+			TaskManager: TaskManagerSpec{
+				Replicas: 3,
+				Ports: TaskManagerPorts{
+					RPC:   &rpcPort,
+					Data:  &dataPort,
+					Query: &queryPort,
+				},
+			},
+			Job: &JobSpec{
+				JarFile:       "",
+				RestartPolicy: &restartPolicy,
+			},
+		},
+	}
+	var err = validator.ValidateCreate(&cluster)
+	var expectedErr = "job jarFile is unspecified"
+	assert.Equal(t, err.Error(), expectedErr)
+
+	cluster = FlinkCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "mycluster",
+			Namespace: "default",
+		},
+		Spec: FlinkClusterSpec{
+			Image: ImageSpec{
+				Name:       "flink:1.8.1",
+				PullPolicy: corev1.PullPolicy("Always"),
+			},
+			JobManager: JobManagerSpec{
+				Replicas:    &jmReplicas,
+				AccessScope: AccessScope.VPC,
+				Ports: JobManagerPorts{
+					RPC:   &rpcPort,
+					Blob:  &blobPort,
+					Query: &queryPort,
+					UI:    &uiPort,
+				},
+			},
+			TaskManager: TaskManagerSpec{
+				Replicas: 3,
+				Ports: TaskManagerPorts{
+					RPC:   &rpcPort,
+					Data:  &dataPort,
+					Query: &queryPort,
+				},
+			},
+			Job: &JobSpec{
+				JarFile:       "gs://my-bucket/myjob.jar",
+				RestartPolicy: &restartPolicy,
+			},
+		},
+	}
+	err = validator.ValidateCreate(&cluster)
+	expectedErr = "job parallelism is unspecified"
+	assert.Equal(t, err.Error(), expectedErr)
+
+	cluster = FlinkCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "mycluster",
+			Namespace: "default",
+		},
+		Spec: FlinkClusterSpec{
+			Image: ImageSpec{
+				Name:       "flink:1.8.1",
+				PullPolicy: corev1.PullPolicy("Always"),
+			},
+			JobManager: JobManagerSpec{
+				Replicas:    &jmReplicas,
+				AccessScope: AccessScope.VPC,
+				Ports: JobManagerPorts{
+					RPC:   &rpcPort,
+					Blob:  &blobPort,
+					Query: &queryPort,
+					UI:    &uiPort,
+				},
+			},
+			TaskManager: TaskManagerSpec{
+				Replicas: 3,
+				Ports: TaskManagerPorts{
+					RPC:   &rpcPort,
+					Data:  &dataPort,
+					Query: &queryPort,
+				},
+			},
+			Job: &JobSpec{
+				JarFile:       "gs://my-bucket/myjob.jar",
+				Parallelism:   &parallelism,
+				RestartPolicy: &invalidRestartPolicy,
+			},
+		},
+	}
+	err = validator.ValidateCreate(&cluster)
+	expectedErr = "invalid job restartPolicy: XXX"
+	assert.Equal(t, err.Error(), expectedErr)
+}
+
 func TestUpdateStatusAllowed(t *testing.T) {
 	var oldCluster = FlinkCluster{Status: FlinkClusterStatus{State: "NoReady"}}
 	var newCluster = FlinkCluster{Status: FlinkClusterStatus{State: "Running"}}
-	var err = validateUpdate(&oldCluster, &newCluster)
+	var validator = &Validator{}
+	var err = validator.ValidateUpdate(&oldCluster, &newCluster)
 	assert.NilError(t, err, "updating status failed unexpectedly")
 }
 
-// Tests updating spec is not allowed.
 func TestUpdateSpecNotAllowed(t *testing.T) {
 	var oldCluster = FlinkCluster{Spec: FlinkClusterSpec{Image: ImageSpec{Name: "flink:1.8.1"}}}
 	var newCluster = FlinkCluster{Spec: FlinkClusterSpec{Image: ImageSpec{Name: "flink:1.9.0"}}}
-	var err = validateUpdate(&oldCluster, &newCluster)
+	var validator = &Validator{}
+	var err = validator.ValidateUpdate(&oldCluster, &newCluster)
 	var expectedErr = "updating FlinkCluster spec is not allowed," +
 		" please delete the resouce and recreate"
 	assert.Equal(t, err.Error(), expectedErr)
