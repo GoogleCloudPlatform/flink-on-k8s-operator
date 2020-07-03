0ca3916a68bf32235c60e480b9e917f94ed24400
xuzhonghu <xuzhonghu@huawei.com>
2020-07-03 09:10:10 +0800

    Fix build

--- controllers/flinkcluster_reconciler.go
+++ controllers/flinkcluster_reconciler.go
@@ -70,8 +70,8 @@ func (reconciler *ClusterReconciler) reconcile() (ctrl.Result, error) {
 		reconciler.log.Info("The cluster update is in progress")
 	}
 	// If batch-scheduling enabled
-	if reconciler.observed.cluster.Spec.BatchSchedulerName != "" {
-		scheduler, err := batchscheduler.GetScheduler(reconciler.observed.cluster.Spec.BatchSchedulerName)
+	if reconciler.observed.cluster.Spec.BatchSchedulerName != nil {
+		scheduler, err := batchscheduler.GetScheduler(*reconciler.observed.cluster.Spec.BatchSchedulerName)
 		if err != nil {
 			return ctrl.Result{}, err
 		}
